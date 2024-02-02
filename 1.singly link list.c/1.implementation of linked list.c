242d77407cc846a21fcf5875267fea5048ac2556
chintu46306 <crg46306@gmail.com>
2024-02-03 01:29:04 +0530

    added

---
+++ 1.singly link list.c/1.implementation of linked list.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+    
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+struct node *head, *newnode; 
+
+void create()
+{
+head=0;
+struct node *temp;
+int choice = 1 ;
+
+
+while(choice)
+  {
+    newnode = (struct node*)malloc(sizeof(struct node));
+    
+    printf("enter the data:\n");
+    scanf("%d",&newnode->data);
+    
+    newnode->next=0;
+    
+    if(head==0)
+    {
+        head=temp=newnode;
+    }
+    else
+    {
+        temp->next=newnode;
+        temp=newnode;
+    }
+    printf("do you want to cantinue(0,1)");
+    scanf("%d",&choice);
+   }
+}
+
+
+void display()
+  {
+    
+struct node *temp;
+temp=head;
+while(temp!=0)
+    {
+       printf("%d \t",temp->data);
+       temp=temp->next;
+    }
+  }
+  int main(){
+      create();
+      display();
+      return 0;
+  }
+
